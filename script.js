f9bab1595adb86ddfe858e8a8e7f72bfeb51630f
andi-kelmendi <andi-kelmendi@hotmail.com>
2024-11-22 22:18:07 +0100

    feat(index) commit

---
+++ script.js
@@ -0,0 +1,47 @@
+// PARTIE page ecriture pour le site
+
+const sidebar = document.querySelector('.sidebar');
+const toggleBtn = document.querySelector('.toggle-btn');
+
+toggleBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+})
+
+
+
+
+
+
+
+
+
+
+// PARIE POUR LA CONNEXION DU SITE
+
+
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+/*const iconClose = document.querySelector('.');*/
+
+registerLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
+});
+
+/*.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
+});*/
