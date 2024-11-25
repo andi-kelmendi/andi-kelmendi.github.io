d97210360b0e4e148eeaa7289b13742a561b8cc5
andi-kelmendi <andi-kelmendi@hotmail.com>
2024-11-25 11:24:10 +0100

    feat(index) commit

--- script.js
+++ script.js
@@ -12,32 +12,32 @@ toggleBtn.addEventListener('click', () => {
 
 
 
-// // PARIE POUR LA CONNEXION DU SITE
+// PARIE POUR LA CONNEXION DU SITE
 
 
-// const wrapper = document.querySelector('.wrapper');
-// const loginLink = document.querySelector('.login-link');
-// const registerLink = document.querySelector('.register-link');
-// const btnPopup = document.querySelector('.btnLogin-popup');
-// const iconClose = document.querySelector('.icon-close');
-// /*const iconClose = document.querySelector('.');*/
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+/*const iconClose = document.querySelector('.');*/
 
-// registerLink.addEventListener('click', ()=> {
-//     wrapper.classList.add('active');
-// });
+registerLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+});
 
-// loginLink.addEventListener('click', ()=> {
-//     wrapper.classList.remove('active');
-// });
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+});
 
-// btnPopup.addEventListener('click', ()=> {
-//     wrapper.classList.add('active-popup');
-// });
+btnPopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');
+});
 
-// iconClose.addEventListener('click', ()=> {
-//     wrapper.classList.remove('active-popup');
-// });
+iconClose.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
+});
 
-// /*.addEventListener('click', ()=> {
-//     wrapper.classList.remove('active-popup');
-// });*/
+/*.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
+});*/
